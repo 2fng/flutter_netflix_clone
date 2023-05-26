9833b626dd505205a6f5ad109c9bc3fa19913835
tunghs-1253 <hua.son.tung@sun-asterisk.com>
2023-05-26 16:51:08 +0700

    HomeScreenHeader UI

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        scaffoldBackgroundColor: Colors.black,
+        textTheme: Theme.of(context).textTheme.apply(bodyColor: Colors.white),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -30,40 +32,152 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        backgroundColor: Colors.transparent,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search_sharp),
+            color: Colors.white,
+            iconSize: 32,
+          )
+        ],
+        titleTextStyle: const TextStyle(color: Colors.white),
       ),
-      body: Center(
+      body: const Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            HomeScreenHeader(),
+            Expanded(
+              child: Center(
+                child: Text('Hehe'),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    );
+  }
+}
+
+class HomeScreenHeader extends StatelessWidget {
+  const HomeScreenHeader({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height / 2.5,
+      alignment: Alignment.center,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage('assets/images/demoBanner.webp'),
+            fit: BoxFit.cover),
       ),
+      child: Stack(children: [
+        Positioned.fill(
+          child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Colors.black.withOpacity(0.8),
+                  Colors.transparent,
+                  Colors.black.withOpacity(0.5),
+                  Colors.black.withOpacity(1)
+                ],
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Demon Slayer',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.w700,
+                    fontFamily: 'Lora'),
+              ),
+              const SizedBox(height: 16),
+              const Text('Anime • Action • Adventure'),
+              const SizedBox(height: 2),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      IconButtonWithTextBelow(
+                        icon: const Icon(
+                          Icons.check,
+                          color: Colors.white,
+                        ),
+                        title: 'My list',
+                        onPress: () {
+                          // ignore: avoid_print
+                          print('My list tapped!');
+                        },
+                      )
+                    ],
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () {},
+                    label: const Text('Play'),
+                    icon: const Icon(Icons.play_arrow),
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(4)),
+                      foregroundColor: Colors.black,
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      IconButtonWithTextBelow(
+                        icon: const Icon(
+                          Icons.info_outline,
+                          color: Colors.white,
+                        ),
+                        title: 'Info',
+                        onPress: () {
+                          // ignore: avoid_print
+                          print('Info tapped!');
+                        },
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ]),
+    );
+  }
+}
+
+class IconButtonWithTextBelow extends StatelessWidget {
+  final String title;
+  final Icon icon;
+  final Function()? onPress;
+  const IconButtonWithTextBelow(
+      {super.key, required this.title, required this.icon, this.onPress});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [IconButton(onPressed: onPress, icon: icon), Text(title)],
     );
   }
 }
